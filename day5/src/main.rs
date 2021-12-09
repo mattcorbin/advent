d5db7902d2f72feb39d87f1edd132aea1342c801
Matt Corbin <mcorbin93@gmail.com>
2021-12-09 14:29:29 -0500

    update formatting and better setup

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -1,3 +1,15 @@
+use std::fs;
+
+fn part1(input: &str) {
+    println!("part1: {}", 0)
+}
+
+fn part2(input: &str) {
+    println!("part2: {}", 0)
+}
+
 fn main() {
-    println!("Hello, world!");
+    let input = fs::read_to_string("input.txt").expect("input.txt should exist");
+    part1(&input);
+    part2(&input);
 }
